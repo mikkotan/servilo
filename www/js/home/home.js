8de0935004368dc8f5a05805c28c4214bb8f2b70
JMatthew Gasataya <jigsthegreat346@gmail.com>
2016-09-22 15:14:24 +0800

    fixed plugin for allow access on multiple image upload

--- www/js/home/home.js
+++ www/js/home/home.js
@@ -58,19 +58,41 @@ function($scope, $ionicModal, $firebaseArray, currentAuth, Restaurant, Home, $st
       height: 800,
       quality: 80
     };
+    window.imagePicker.getPictures(
+        function(results) {
+            for (var i = 0; i < results.length; i++) {
+                console.log('Image URI: ' + results[i]);
+                window.plugins.Base64.encodeFile(results[i], function(base64){
+                    $scope.images.push(base64);
+                    $scope.$apply();
+                });
+            }
+        }, function (error) {
+            console.log('Error: ' + error);
+        }, {
+            maximumImagesCount: 10,
+            width: 800,
+            quality: 80,
+          // output type, defaults to FILE_URIs.
+          // available options are 
+          // window.imagePicker.OutputType.FILE_URI (0) or 
+          // window.imagePicker.OutputType.BASE64_STRING (1)
+            // outputType: window.imagePicker.OutputType.BASE64_STRING
+        }
+    );
 
-    $cordovaImagePicker.getPictures(options)
-    .then(function (results) {
-      for (var i = 0; i < results.length; i++) {
-        window.plugins.Base64.encodeFile(results[i], function(base64){
-          $scope.images.push(base64);
-          $scope.$apply();
-        });
-        console.log('Image URI: ' + results[i]);
-      }
-    }, function(error) {
-      // error getting photos
-    });
+    // $cordovaImagePicker.getPictures(options)
+    // .then(function (results) {
+    //   for (var i = 0; i < results.length; i++) {
+    //     window.plugins.Base64.encodeFile(results[i], function(base64){
+    //       $scope.images.push(base64);
+    //       $scope.$apply();
+    //     });
+    //     console.log('Image URI: ' + results[i]);
+    //   }
+    // }, function(error) {
+    //   // error getting photos
+    // });
   };
 
   $scope.addReview = function(review) {
@@ -85,7 +107,8 @@ function($scope, $ionicModal, $firebaseArray, currentAuth, Restaurant, Home, $st
       restaurant_id : id,
       timestamp: firebase.database.ServerValue.TIMESTAMP
     }).then(function(review) {
-      var newImages = newReviewRef.child(review.key).child('images');
+      $ionicLoading.hide();
+      var newImages = Database.reviewsReference().child(review.key).child('images');
       var list = $firebaseArray(newImages);
       for (var i = 0; i < $scope.images.length; i++) {
         list.$add({ image: $scope.images[i] }).then(function(ref) {
@@ -97,7 +120,6 @@ function($scope, $ionicModal, $firebaseArray, currentAuth, Restaurant, Home, $st
       // Database.restaurantsReference().child(id).child('reviews').child(review.key).set(true); //new
       // Database.restaurantsReference().child(id).child('reviewers').child(User.auth().$id).set(true); //new
       $scope.isAlreadyReviewed();
-      $ionicLoading.hide();
       $scope.reviewModal.hide();
       review.content = '';
       $scope.review.rating = 0;
@@ -107,6 +129,7 @@ function($scope, $ionicModal, $firebaseArray, currentAuth, Restaurant, Home, $st
   }
 
   $scope.updateReview = function(review) {
+    // console.log("hiiii");
     var reviewRef = Database.reviewsReference().child(review.$id);
     var reviewRating = review.rating;
     reviewRef.update({
