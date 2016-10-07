3dc0bfea9c91e26f30904b4f291b59fe248f4a38
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2016-10-07 10:54:46 +0800

    Menus with Pictures

--- www/js/menu/addMenu.js
+++ www/js/menu/addMenu.js
@@ -1,7 +1,38 @@
-app.controller("AddMenuCtrl",["$scope","$stateParams","menus","$firebaseArray","$firebaseObject","$state","restaurantId",
-  function($scope ,$stateParams,menus,$firebaseArray,$firebaseObject,$state,restaurantId){
+app.controller("AddMenuCtrl",["$scope","$stateParams","menus","$firebaseArray","$firebaseObject","$state","restaurantId","$cordovaCamera",
+  function($scope ,$stateParams,menus,$firebaseArray,$firebaseObject,$state,restaurantId, $cordovaCamera){
 
     $scope.menus = menus;
+
+    $scope.imageURL = "";
+
+    $scope.upload = function(index) {
+      var source = "";
+      switch(index) {
+        case 1:
+          source = Camera.PictureSourceType.CAMERA;
+          break;
+        case 2:
+          source = Camera.PictureSourceType.PHOTOLIBRARY;
+          break;
+      }
+      var options = {
+        quality : 75,
+        destinationType : Camera.DestinationType.DATA_URL,
+        sourceType : source,
+        allowEdit : true,
+        encodingType: Camera.EncodingType.JPEG,
+        popoverOptions: CameraPopoverOptions,
+        targetWidth: 500,
+        targetHeight: 500,
+        saveToPhotoAlbum: false
+      };
+      $cordovaCamera.getPicture(options).then(function(imageData) {
+        $scope.imageURL = imageData;
+
+        }, function(error) {
+          console.error(error);
+        });
+    }
 
     $scope.addMenu = function(menu){
     $scope.menus.$add({
@@ -9,6 +40,7 @@ app.controller("AddMenuCtrl",["$scope","$stateParams","menus","$firebaseArray","
       price : menu.price,
       restaurant_id : restaurantId,
       prevPrice : menu.price,
+      image : $scope.imageURL,
       timestamp : firebase.database.ServerValue.TIMESTAMP
     }).then(function(menuObj){
       Database.restaurantsReference().child(restaurantId).child("menus").child(menuObj.key).set(true);
