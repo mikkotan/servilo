0f398ae1b823bdde8ca79b089a6cbabc17c987b1
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2016-10-23 18:42:57 +0800

    Notifications individual delete

--- www/app/notification/notifications.controller.js
+++ www/app/notification/notifications.controller.js
@@ -3,9 +3,27 @@ app.controller("NotificationsCtrl", ["$scope", "$firebaseArray", "User", "Restau
 
     $scope.notifs = notifications;
 
+    $scope.markReadAll = function() {
+      angular.forEach(notifications, function(notif) {
+        console.log(notif);
+      })
+    }
+
+    $scope.markRead = function(notif) {
+      notifications.$remove(notif)
+        .then(() => {
+          console.log("success")
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+
     $scope.$watchCollection('notifs', function(newNotifs) {
       $scope.newNotifs = newNotifs.map(function(notification) {
         return {
+          self : notification,
+          id : notification.$id,
           sender_id : notification.sender_id,
           status : notification.status,
           order_no : notification.order_no,
