cffd6aa0e68d0e45cfe3cda63a8e001d2e9c902e
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2016-11-15 21:00:58 +0800

    User Cancel Reservation

--- www/app/reservation/my-reservations.controller.js
+++ www/app/reservation/my-reservations.controller.js
@@ -1,5 +1,5 @@
-app.controller('MyReservationsCtrl',["$scope", "reservations", "Restaurant",
-  function($scope, reservations, Restaurant) {
+app.controller('MyReservationsCtrl',["$scope", "reservations", "Restaurant", "Database", "$ionicPopup",
+  function($scope, reservations, Restaurant, Database, $ionicPopup) {
     console.log('my reservations ctrl launched');
     $scope.test = "hello world";
 
@@ -17,13 +17,22 @@ app.controller('MyReservationsCtrl',["$scope", "reservations", "Restaurant",
     })
 
     $scope.cancel = function(reservation) {
-      alert(reservation.$id);
+      var cancelPopup = $ionicPopup.confirm({
+        title: "Cancel Reservation",
+        template: "Are you sure to cancel your reservation for '" + Restaurant.get(reservation.restaurant_id).name + "' ?"
+      })
+
+      cancelPopup
+        .then((res) => {
+          if (res) {
+            var ref = Database.reservationsReference().child(reservation.$id).child('status')
+            ref.set('cancelled');
+          }
+          else {
+            console.log('Cancellation failed');
+          }
+        })
+
     }
 
-    // $scope.reservations = reservations.map(function(reservation) {
-    //   return {
-    //     details: reservation,
-    //     restaurant: Restaurant.get(reservation.restaurant_id)
-    //   }
-    // });
   }])
