99b5556db213fc0be682bc627d2381de8194eb10
John Matthew Labtic <matthewlabtic@gmail.com>
2016-11-29 11:09:26 +0800

    fix app directive

--- www/js/app.js
+++ www/js/app.js
@@ -104,10 +104,10 @@ app.run(["$ionicPlatform", "$rootScope", "$state", '$templateCache', "IonicPushS
       }
     })
 
-    $rootScope.$on('$cordovaPushV5:errorOcurred', function(event, e){
-       // e.message
-       console.log(e.message);
-     });
+    $rootScope.$on('$cordovaPushV5:errorOcurred', function(event, e) {
+      // e.message
+      console.log(e.message);
+    });
 
     $rootScope.$on("$stateChangeError",
 
@@ -196,28 +196,20 @@ app.controller('AppCtrl', function($scope, $ionicLoading, $ionicSideMenuDelegate
 
 app.directive('googleplace', function() {
   return {
-        require: 'ngModel',
-        scope: {
-            ngModel: '=',
-            details: '=?'
-        },
-        link: function(scope, element, attrs, model) {
-          // this city bounds does not limit the search but biasing the search
-          var cityBounds = new google.maps.LatLngBounds(
-              new google.maps.LatLng(10.689760946107592, 122.43714093987364),
-              new google.maps.LatLng(10.851652605488333, 122.63352155510802));
-            var options = {
-              bounds: cityBounds,
-              componentRestrictions: {country: 'PH'}
-            };
-            scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
-
-            google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
-              scope.$apply(function() {
-                scope.details = scope.gPlace.getPlace().geometry.location;
-                model.$setViewValue(element.val());
-              });
-            });
+    require: 'ngModel',
+    scope: {
+      ngModel: '=',
+      details: '=?'
+    },
+    link: function(scope, element, attrs, model) {
+      // this city bounds does not limit the search but biasing the search
+      var cityBounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(10.689760946107592, 122.43714093987364),
+        new google.maps.LatLng(10.851652605488333, 122.63352155510802));
+      var options = {
+        bounds: cityBounds,
+        componentRestrictions: {
+          country: 'PH'
         }
       };
       scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
@@ -228,6 +220,15 @@ app.directive('googleplace', function() {
           model.$setViewValue(element.val());
         });
       });
+
     }
   };
+  scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
+
+  google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
+    scope.$apply(function() {
+      scope.details = scope.gPlace.getPlace().geometry.location;
+      model.$setViewValue(element.val());
+    });
+  });
 });
