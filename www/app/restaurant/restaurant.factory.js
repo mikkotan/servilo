ecb0f0965b1f36dc131b04750c795aa8f05d9ec3
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2016-12-16 18:08:59 +0800

    category_restaurants

--- www/app/restaurant/restaurant.factory.js
+++ www/app/restaurant/restaurant.factory.js
@@ -14,7 +14,6 @@ app.factory("Restaurant", ["$firebaseArray", "User", "Database", "$firebaseObjec
     var Restaurant = {
       all: function() {
         return Database.restaurants();
-
       },
       getAuthUserRestaurants: function() {
         var authUserId = User.auth().$id;
@@ -25,7 +24,6 @@ app.factory("Restaurant", ["$firebaseArray", "User", "Database", "$firebaseObjec
       },
       getPendingRestaurants: function() {
         return Database.pendings();
-        // return $firebaseArray(Database.pendingsReference())
       },
       getAverageRating: function(restaurantId) {
         console.log('getting averge rating to ' + restaurantId);
@@ -66,20 +64,11 @@ app.factory("Restaurant", ["$firebaseArray", "User", "Database", "$firebaseObjec
       getReservations: function(restaurantId) {
         return $firebaseArray(Database.restaurantReservationsReference().child(restaurantId));
       },
-      // getOrders : function(restaurantId) {
-      //   console.log('getting orders in restaurant factory');
-      //   return $firebaseArray(restaurantOrders.child(restaurantId))
-      // },
       getReviews: function(restaurantId) {
-        // return $firebaseArray(reviews.orderByChild('restaurant_id').equalTo(restaurantId));
         return $firebaseArray(restaurantReviews.child(restaurantId).limitToLast(5))
       },
       getOwner: function(restaurantId) {
-        // console.log('get owner method');
-        // return $firebaseObject(users.child(ownerId));
-        console.log(restaurantId);
         var res = restaurantsArray.$getRecord(restaurantId);
-        console.log(res);
         return $firebaseObject(users.child(res.owner_id))
       },
       getOrders: function(restaurantId) {
@@ -194,11 +183,24 @@ app.factory("Restaurant", ["$firebaseArray", "User", "Database", "$firebaseObjec
         })
       },
       addRestaurant: function(restaurant) {
+
+        console.log('adding restaurant here @@@@@@@@@@@@@@@');
+        console.log(restaurant.categories);
         delete restaurant['$id'];
         delete restaurant['$priority'];
         delete restaurant['$$hashKey'];
         var key = restaurants.push().key;
         var restaurantRef = restaurants.child(key);
+
+        for (var category in restaurant.categories) {
+          console.log(category)
+          console.log(restaurant.categories[category])
+          if (restaurant.categories[category]) {
+            Database.categoryRestaurantsReference().child(category).child(key).set(true)
+              .then(() => { console.log('success on category adding' )})
+              .catch((err) => { console.log('Error'+ err) })
+          }
+        }
         console.log(restaurant.categories);
         return {
           ref: restaurantRef.set(restaurant),
@@ -216,7 +218,6 @@ app.factory("Restaurant", ["$firebaseArray", "User", "Database", "$firebaseObjec
           facilities: restaurant.facilities,
           categories: restaurant.categories,
           openDays: restaurant.openDays,
-          categories: "wa",
           location: location,
           latitude: lat,
           longitude: long,
