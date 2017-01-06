aa8b3bc21ba9bb59ead9ef0972668dc611c9dab5
John Matthew Labtic <matthewlabtic@gmail.com>
2017-01-07 04:32:05 +0800

    slight sidebar change, changes in dashboard-interact

--- www/app/dashboard/dashboard-interact.controller.js
+++ www/app/dashboard/dashboard-interact.controller.js
@@ -1,25 +1,33 @@
-app.controller('DashboardInteractCtrl', ['$scope', '$state', '$stateParams',
-  function($scope, $state, $stateParams) {
+app.controller('DashboardInteractCtrl', ['$scope', '$state', '$stateParams', "ionicMaterialInk", "ionicMaterialMotion",
+  function($scope, $state, $stateParams, ionicMaterialInk, ionicMaterialMotion) {
     console.log('DashboardInteract Ctrl run');
-
+    ionicMaterialInk.displayEffect();
     console.log($stateParams.restaurantId);
     $scope.goToOrders = function() {
-      $state.go('tabs.dashboard.orders', {restaurantId: $stateParams.restaurantId});
+      $state.go('tabs.dashboard.orders', {
+        restaurantId: $stateParams.restaurantId
+      });
     }
 
     $scope.goToReservations = function() {
       console.log('click go to reservations');
-      $state.go('tabs.dashboard.reservations', {restaurantId: $stateParams.restaurantId});
+      $state.go('tabs.dashboard.reservations', {
+        restaurantId: $stateParams.restaurantId
+      });
     }
 
     $scope.goToReviews = function() {
       console.log('click go to reviews');
-      $state.go('tabs.dashboard.reviews', {restaurantId: $stateParams.restaurantId});
+      $state.go('tabs.dashboard.reviews', {
+        restaurantId: $stateParams.restaurantId
+      });
     }
 
     $scope.goToPromos = function() {
       console.log('click go to promos');
-      $state.go('tabs.dashboard.promos', {restaurantId: $stateParams.restaurantId});
+      $state.go('tabs.dashboard.promos', {
+        restaurantId: $stateParams.restaurantId
+      });
     }
   }
 ])
