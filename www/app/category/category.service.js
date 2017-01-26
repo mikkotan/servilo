883e0d2a6a5b87ee4dbbdf0accdf73f19e989cd5
Michael Rasheed Tan <m_rasheed_tan@yahoo.com>
2017-01-26 06:21:16 +0000

    category.service.js edited online with Bitbucket

--- www/app/category/category.service.js
+++ www/app/category/category.service.js
@@ -1,11 +1,25 @@
-app.factory('Category', function(Database, $firebaseArray) {
-  var Category = {
-    getAllCategories : function() {
-      return $firebaseArray(Database.categoryRestaurantsReference())
-    },
-    getRestaurants : function(category) {
-      return $firebaseArray(Database.categoryRestaurantsReference().child(category).child('restaurants'))
+//app.factory('Category', function(Database, $firebaseArray) {
+//  var Category = {
+//    getAllCategories : function() {
+//      return $firebaseArray(Database.categoryRestaurantsReference())
+//    },
+//    getRestaurants : function(category) {
+//      return $firebaseArray(Database.categoryRestaurantsReference().child(category).child('restaurants'))
+//    }
+//  }
+//  return Category
+//})
+
+app.factory('Category', ['Database', '$firebaseArray', 
+  function(Database, $firebaseArray) {
+    var Category = {
+      getAllCategories : function() {
+        return $firebaseArray(Database.categoryRestaurantsReference())
+      },
+      getRestaurants : function(category) {
+        return $firebaseArray(Database.categoryRestaurantsReference().child(category).child('restaurants'))
+      }
     }
-  }
-  return Category
-})
+    return Category
+  }  
+])
